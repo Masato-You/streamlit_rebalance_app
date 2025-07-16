ff9d6315775aefa0bcae844c3933a6619a0b49b0
Cube <yashan1023@gmail.com>
2025-07-16 23:59:45 +0800

    fillna(1)

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -274,7 +274,7 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
     
     try:
         # 使用 start 和 end 參數獲取指定區間數據
-        asset_prices_hist = yf.Tickers(' '.join(tickers_list)).history(period="5y", interval="1d", back_adjust=True)['Close'].ffill().fillna(0)
+        asset_prices_hist = yf.Tickers(' '.join(tickers_list)).history(period="5y", interval="1d", back_adjust=True)['Close'].ffill().fillna(1)
         
         if asset_prices_hist.empty:
             raise ValueError("無法獲取任何資產的歷史價格。")
