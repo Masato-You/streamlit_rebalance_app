80d071947854dd95bed054ffe8b0b1db65914439
Cube <yashan1023@gmail.com>
2025-07-17 23:21:48 +0800

    %%

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -373,7 +373,7 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
     )
     fig_perf.update_xaxes(showspikes=True, spikecolor="white", spikesnap="cursor", spikemode="across+marker")
     fig_perf.update_yaxes(showspikes=True, spikecolor="white", spikethickness=2)
-    fig_perf.update_traces(hovertemplate='<extra></extra>%{y:.2f}')
+    fig_perf.update_traces(hovertemplate='<extra></extra>%{y:.2f}%%')
     return fig_value, fig_perf
 
 
