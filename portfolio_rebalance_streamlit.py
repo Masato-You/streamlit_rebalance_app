f8fce02f40ae7141f41e536d85636f76b0cced87
Cube <yashan1023@gmail.com>
2025-07-18 21:52:35 +0800

    x unified

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -339,7 +339,7 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
         title=f'投資組合近{option_map[option]}總資產走勢 (以台幣計價)',
         yaxis_title='總資產價值 (TWD)', xaxis_title='日期',
         template='plotly_dark', height=500, yaxis_tickformat=',.0f',
-        yaxis=dict(range=[y_min, y_max]), hovermode="x"
+        yaxis=dict(range=[y_min, y_max]), hovermode="x unified"
     )
     fig_value.update_xaxes(showspikes=True, spikecolor="white", spikesnap="cursor", spikemode="across+marker")
     fig_value.update_yaxes(showspikes=True, spikecolor="white", spikethickness=1.5)
@@ -369,7 +369,7 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
         title='投資組合累積績效 (%)',
         yaxis_title='績效 (%)', xaxis_title='日期',
         template='plotly_dark', height=500,
-        yaxis_ticksuffix=' %', hovermode="x"
+        yaxis_ticksuffix=' %', hovermode="x unified"
     )
     fig_perf.update_xaxes(showspikes=True, spikecolor="white", spikesnap="cursor", spikemode="across+marker")
     fig_perf.update_yaxes(showspikes=True, spikecolor="white", spikethickness=1.5)
