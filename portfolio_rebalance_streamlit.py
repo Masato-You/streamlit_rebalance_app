8e05bb3c36922fe894c78b89f8b582f6e2a469db
Cube <yashan1023@gmail.com>
2025-07-20 12:07:47 +0800

    height=700

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -498,7 +498,7 @@ def create_polar_comparison_charts(
         fig.update_layout(
             title=title,
             template='plotly_dark',
-            height=600,
+            height=700,
             polar=dict(
                 radialaxis=dict(
                     visible=False,
