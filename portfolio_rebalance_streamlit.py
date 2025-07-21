d11eba513481ee52bc9509cb7042ef3526cb6bb4
Cube <yashan1023@gmail.com>
2025-07-21 20:06:41 +0800

    base_radius

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -432,7 +432,8 @@ def create_polar_comparison_charts(
 
         # 新增一個代表 100% 目標的基準線環
         fig.add_trace(go.Scatterpolar(
-            r=np.ones(120) * Radius,
+            r=np.ones(120) * Radius-9.5,
+            base_radius = 9.5,
             theta=np.linspace(0, 360, 120),
             mode='lines',
             name='目標基準',
