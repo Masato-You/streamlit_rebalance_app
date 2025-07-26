775ab80e79c7b43994aa1f583e60037fa0ccd055
Cube <yashan1023@gmail.com>
2025-07-26 22:18:01 +0800

    gradient  "rgba(29,66,131,1)"

--- portfolio_rebalance_streamlit.py
+++ portfolio_rebalance_streamlit.py
@@ -336,7 +336,7 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
     fig_value.add_trace(go.Scatter(
         x=total_portfolio_value_oneyear.index, y=total_portfolio_value_oneyear,
         mode='lines', name='總資產', line=dict(color='deepskyblue', width=2), fill='tozeroy',
-        fillgradient=dict(colorscale='blues', type='vertical', start = gradient_start*0.6, stop = gradient_start), showlegend=False))
+        fillgradient=dict(colorscale=[(0.0, "rgba(29,66,131,1)"), (0.5,  "rgba(29,66,131,0.5)"), (1.0,  "rgba(29,66,131,0)")], type='vertical', start = gradient_start*0.6, stop = gradient_start), showlegend=False))
     y_min = total_portfolio_value_oneyear.min() * 0.98
     y_max = total_portfolio_value_oneyear.max() * 1.02
     fig_value.update_layout(
@@ -345,8 +345,8 @@ def create_portfolio_charts(tickers_list: list, quantities_array: np.ndarray, as
         template='plotly_dark', height=500, yaxis_tickformat=',.0f',
         yaxis=dict(range=[y_min, y_max]), hovermode=None
     )
-    fig_value.update_xaxes(showspikes=True, spikecolor="white", spikesnap="cursor", spikemode="across+marker", spikedash="dot", spikethickness=1.5)
-    fig_value.update_yaxes(showspikes=True, spikecolor="white", spikethickness=1.5, spikedash="dot", spikemode="across+marker")
+    fig_value.update_xaxes(showspikes=True, spikecolor="gray", spikesnap="cursor", spikemode="across+marker", spikedash="dot", spikethickness=0.5)
+    fig_value.update_yaxes(showspikes=True, spikecolor="gray", spikethickness=0.5, spikedash="dot", spikemode="across+marker")
     fig_value.update_traces(hovertemplate='    %{x}<br>'+
                             '    NT$%{y}')
     # --- 圖表二：累積績效 (%) ---
